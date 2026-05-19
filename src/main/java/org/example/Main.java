fe935ea1224582949a18fefed2d8312dc9587c78
Mikołaj Michnik <mikmic.win@gmail.com>
2026-05-19 18:38:59 +0200

    pokaz dla igora

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     static void main() {
         System.out.println(("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             System.out.println("i = " + i);
         }
     }
