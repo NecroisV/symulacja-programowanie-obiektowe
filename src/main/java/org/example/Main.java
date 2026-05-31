85bfb8ae5e945397f40e5c631e03a6949b131843
Mikołaj Michnik <mikmic.win@gmail.com>
2026-06-01 01:55:22 +0200

    dodanie wizji, wag poruszania się, walki, interakcji międziy agentami oraz gui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         RNG.initRNG(SimulationParameters.getInstance().getSimulationSeed());
         SimulationApp.main(args);
     }
